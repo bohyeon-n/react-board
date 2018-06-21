48c8d0b4f8805fe1748c95722e212a4cf6c12871
koobohyeon <goosee115@gmail.com>
2018-06-21 18:41:25 +0900

    add router

--- src/App.js
+++ src/App.js
@@ -3,13 +3,19 @@ import logo from "./logo.svg";
 import "./App.css";
 import LoginPage from "./pages/LoginPage";
 import PostPage from "./pages/PostPage";
+import {BrowserRouter, Route, Redirect} from 'react-router-dom'
+
 class App extends Component {
   render() {
     return (
-      <div>
-        <LoginPage />
-        <PostPage />
-      </div>
+      <BrowserRouter>
+      <React.Fragment>
+
+        <Route path='/login' component={LoginPage}/>
+        <Route path='/' component={PostPage}/>
+
+      </React.Fragment>
+      </BrowserRouter>      
     );
   }
 }
